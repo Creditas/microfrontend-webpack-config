3f63978752ce5ee021061e5f5c8a4c1740062d96
Caio Abe <caio.abe@estilo7.com.br>
2019-11-01 08:56:02 -0300

    Add env.test

--- src/helpers.js
+++ src/helpers.js
@@ -4,14 +4,17 @@ const isFn = fn => typeof fn === 'function'
 const isObj = obj => typeof obj === 'object'
 const isNotObjFn = obj => !isObj(obj) && !isFn(obj)
 
-const isStg = value => value === 'staging'
 const isDev = value => value === 'development'
+const isTest = value => value === 'test'
+const isStg = value => value === 'staging'
 const isProd = value => value === 'production'
 const log = debug => message => debug && console.log(message) //eslint-disable-line no-console
 
 const getEnvFile = environment => {
   if (isDev(environment)) {
     return '.env.development'
+  } else if (isTest(environment)) {
+    return '.env.test'
   } else if (isStg(environment)) {
     return '.env.staging'
   } else if (isProd(environment)) {
